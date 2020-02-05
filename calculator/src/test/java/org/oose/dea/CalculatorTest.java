072ba6754d5a2f94ac4ee6065809ed4f7e5b45ce
Gino Verwijs <46499009+gwijs@users.noreply.github.com>
2020-02-05 10:35:53 +0100

    Update CalculatorTest.java

--- calculator/src/test/java/org/oose/dea/CalculatorTest.java
+++ calculator/src/test/java/org/oose/dea/CalculatorTest.java
@@ -2,13 +2,38 @@ package org.oose.dea;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    public void makeCalculator(){
+        calculator = new Calculator();
+    }
+
     @Test
     public void testAdd(){
-        Calculator calculator = new Calculator ();
         assertEquals(4,calculator.add(2,2));
         assertEquals(100,calculator.add(25,75));
+    }
+
+    @Test
+    public void testSubtract(){
+        assertEquals(4, calculator.subtract(8,4));
+        assertEquals(100, calculator.subtract(131, 31));
+    }
+
+    @Test
+    public void testTimes(){
+        assertEquals(50, calculator.times(5,10));
+        assertEquals(49, calculator.times(7,7));
+    }
+
+    @Test
+    public void testDivide(){
+        assertEquals(32, calculator.divide(96,3));
+        assertEquals(2, calculator.divide(4,2));
     }
 }
